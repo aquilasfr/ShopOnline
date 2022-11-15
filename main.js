8229eda6972834b56724e5bae8c436300b1246ef
Aquilas França <aquilasfr>
2022-11-14 21:39:16 -0300

    Remove for e adding forEach

--- main.js
+++ main.js
@@ -1,40 +1,45 @@
 const $tableRows = document.querySelectorAll('tbody > tr');
 
-const increment = (element, button) => {
+const increment = (element, button, tableRow) => {
     element.textContent = `${parseInt(element.textContent) + 1}`;
     button.removeAttribute('disabled');
     
-    const $productPrice = document.querySelector('.price');
-    const $subTotal = document.querySelector('.sub-total');
-
-    const $parsedValue = parseInt($productPrice.textContent);
+    const $productPrice = tableRow.querySelector('.price');
+    const $subTotal = tableRow.querySelector('.sub-total');
+    const $totalBuy = document.querySelector('.total');
     
-    const resultMult = $parsedValue * element.textContent;
+    const parsedValue = parseInt($productPrice.textContent);
+    
+    const resultMult = parsedValue * element.textContent;
+    
     $subTotal.textContent = resultMult.toLocaleString('pt-BR', { minimumFractionDigits: 2});
+    $totalBuy.textContent = resultMult.toLocaleString('pt-BR', { minimumFractionDigits: 2});
 }   
-const decrement = (element, button) => {
+const decrement = (element, button, tableRow) => {
     if(element.textContent > 1) {
         element.textContent = parseInt(element.textContent) - 1;
                 
-        const $productPrice = document.querySelector('.price');
-        const $subTotal = document.querySelector('.sub-total');
+        const $productPrice = tableRow.querySelector('.price');
+        const $subTotal = tableRow.querySelector('.sub-total');
+        const $totalBuy = document.querySelector('.total');
 
-        const $parsedValueProd = parseInt($productPrice.textContent);
-        const $parsedValueSub = parseInt($subTotal.textContent);
+        const parsedValue = parseInt($productPrice.textContent);
+        const parsedValueSubtotal = parseInt($subTotal.textContent);
         
-        const resultSub = $parsedValueSub - $parsedValueProd;
+        const resultSub = parsedValueSubtotal - parsedValue;
+        
         $subTotal.textContent = resultSub.toLocaleString('pt-BR', { minimumFractionDigits: 2});
-        console.log($subTotal.textContent)
+        $totalBuy.textContent = resultSub.toLocaleString('pt-BR', { minimumFractionDigits: 2});
     }else {
         button.setAttribute('disabled', '');
     }
 }
 
-for (let i = 0; i < $tableRows.length; i++) {
-    const $incrementButton = $tableRows[i].querySelector('.btn-increment');
-    const $decrementButton = $tableRows[i].querySelector('.btn-decrement');
-    const $amount = $tableRows[i].querySelector('.the-amount');  
-    
-    $incrementButton.addEventListener('click', () => increment($amount, $decrementButton));
-    $decrementButton.addEventListener('click', () => decrement($amount, $decrementButton));
-}
+$tableRows.forEach((tableRow) => {
+    const $incrementButton = tableRow.querySelector('.btn-increment');
+    const $decrementButton = tableRow.querySelector('.btn-decrement');
+    const $amount = tableRow.querySelector('.the-amount');  
+
+    $incrementButton.addEventListener('click', () => increment($amount, $incrementButton, tableRow));
+    $decrementButton.addEventListener('click', () => decrement($amount, $decrementButton, tableRow));
+});
